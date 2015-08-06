a75feed63357f15aeec918d20a7e676fba55c994
myun2 <myun2@nwhite.info>
2015-08-06 18:54:34 +0900

    users

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :portals
+  resources :users, only: [:index, :show]
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
