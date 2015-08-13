7179e89752a541abe79dba3c4c2ac71deb13d96b
myun2 <myun2@nwhite.info>
2015-08-13 16:02:30 +0900

    logging footprint

--- app/models/footprint.rb
+++ app/models/footprint.rb
@@ -1,3 +1,15 @@
 class Footprint < ActiveRecord::Base
   belongs_to :user
+
+  def log!
+    self.logs = (logs || "") + logging_text
+    save!
+  end
+
+  private
+  def logging_text
+    <<-TEXT
+#{location} at #{I18n.l updated_at}. (location: #{longitude}, #{latitude})
+    TEXT
+  end
 end
--- app/procedures/mark_footprint.rb
+++ app/procedures/mark_footprint.rb
@@ -1,6 +1,23 @@
 class MarkFootprint
+  attr_reader :user, :params
+
+  def initialize(user, params)
+    @user = user
+    @params = params
+  end
+
   def self.do(user, params)
-    f = Footprint.first_or_initialize(user: user)
-    f.update!(params.permit :latitude, :longitude, :location)
+    f = new(user,params)
+    f.do!
+  end
+
+  def do!
+    footprint.update!(params.permit :latitude, :longitude, :location)
+    footprint.log!
+  end
+
+  private
+  def footprint
+    Footprint.first_or_initialize(user: user)
   end
 end
